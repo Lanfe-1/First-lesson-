42c0dd913cdb207976cc150c75d4a87bf942b29b
Юрий <pointkiller12345@gmail.com>
2024-12-07 11:43:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# \n -> перенос на новую строчку
+# \t -> табуляция
+# \  -> Зеркалирование
+
+print ("hello world1")
+print ("hello\nworld2!")
+print ("hello\n\tworld3")
+print ("hello\"world4\"")
+print ("hello\\n\\t\"world5\"")
+
+####
+
+print ("hello")
+print ("world")
+
+# end= -> добавялет написанное в " " между 2 принтами
+print ("hello", end=" - ")
+print ("world")
+
+# sep= -> установить\убрать пробелы в принте
+
+print ("hello", "one", "two", "three", sep=",")
